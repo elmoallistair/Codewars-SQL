fbe2479639b8304ffa0645ee2692f45dfb32f79d
Elmo Allistair <55618192+elmoallistair@users.noreply.github.com>
2020-05-06 21:05:27 +0700

    Create query.sql

---
+++ SQ Basics - Simple JOIN/query.sql
@@ -0,0 +1,9 @@
+-- Written: 6-May-2020
+-- https://www.codewars.com/kata/5802e32dd8c944e562000020/train/sql
+
+SELECT    
+  p.*,
+  c.name AS company_name
+FROM products AS p
+LEFT JOIN companies AS c
+ON p.company_id = c.id
