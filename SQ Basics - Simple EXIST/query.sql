61747a89f707405823dd3a27703311b4df472f5e
Elmo Allistair <55618192+elmoallistair@users.noreply.github.com>
2020-05-06 22:35:43 +0700

    Create query.sql

---
+++ SQ Basics - Simple EXIST/query.sql
@@ -0,0 +1,13 @@
+-- Written: 06-May-2020
+-- https://www.codewars.com/kata/58113a64e10b53ec36000293/train/sql
+
+SELECT 
+  id, name
+FROM departments
+WHERE EXISTS (
+  SELECT department_id
+  FROM sales
+  WHERE (
+    departments.id = sales.department_id AND price > 98.00
+  )
+)
