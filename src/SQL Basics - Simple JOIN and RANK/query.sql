6ebd3a177d44e9ad2ff9fe38f9a4afba6f3cb9d1
Elmo Allistair <55618192+elmoallistair@users.noreply.github.com>
2020-05-11 23:22:18 +0700

    Create query.sql

---
+++ src/SQL Basics - Simple JOIN and RANK/query.sql
@@ -0,0 +1,12 @@
+-- Written: 11-May-2020
+-- https://www.codewars.com/kata/58094559c47d323ebd000035/train/sql
+
+SELECT
+  p.id,
+  p.name,
+  COUNT(s.sale) as sale_count,
+  RANK() OVER (ORDER BY COUNT(s.sale)) as sale_rank
+FROM people as p
+JOIN sales as s
+  ON p.id = s.people_id
+GROUP BY p.id
